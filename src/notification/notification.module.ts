cfc53e389157e8a701815b60ec0aae1119c418f2
Pranav Acharya <pranavacharya432@gmail.com>
2021-06-02 23:30:31 -0700

    add bull board

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,5 +1,9 @@
 import { BullModule } from '@nestjs/bull';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { createBullBoard } from 'bull-board';
+const { BullAdapter } = require('bull-board/bullAdapter')
+const Queue = require('bull')
+const { BullMQAdapter } = require('bull-board/bullMQAdapter')
 import { EmailConsumer } from './email-consumer';
 import { NotificationService, QueueNames } from './notification.service';
 
@@ -13,4 +17,13 @@ import { NotificationService, QueueNames } from './notification.service';
   providers: [NotificationService, EmailConsumer],
   exports: [NotificationService]
 })
-export class NotificationModule {}
+export class NotificationModule {
+    // https://stackoverflow.com/questions/63271404/nestjs-connect-bull-board-in-a-normal-controller
+    configure(consumer: MiddlewareConsumer): void {
+        const result = createBullBoard([]);
+        result.setQueues([new BullAdapter(new Queue(QueueNames.Email))])
+        consumer
+          .apply(result.router)
+          .forRoutes('/admin/queues');
+      }
+}
